9a01ef3287a9cc8998c769f87624fdb819cf942f
Daniele Lacamera <root@danielinux.net>
2026-03-23 08:43:02 +0100

    zero MAC stack buffer on exit
    F/1202

--- src/psa_mac.c
+++ src/psa_mac.c
@@ -352,6 +352,7 @@ static psa_status_t wolfpsa_mac_final(wolfpsa_mac_ctx_t *ctx,
     uint8_t full_mac[PSA_MAC_MAX_SIZE];
     word32 full_len = 0;
     int ret;
+    psa_status_t status;
 
     if (mac == NULL || mac_length == NULL) {
         return PSA_ERROR_INVALID_ARGUMENT;
@@ -369,7 +370,8 @@ static psa_status_t wolfpsa_mac_final(wolfpsa_mac_ctx_t *ctx,
         full_len = (word32)sizeof(full_mac);
         ret = wc_HmacFinal(&ctx->ctx.hmac, full_mac);
         if (ret != 0) {
-            return wc_error_to_psa_status(ret);
+            status = wc_error_to_psa_status(ret);
+            goto cleanup;
         }
         full_len = (word32)ctx->full_length;
     }
@@ -377,20 +379,27 @@ static psa_status_t wolfpsa_mac_final(wolfpsa_mac_ctx_t *ctx,
         full_len = (word32)ctx->full_length;
         ret = wc_CmacFinal(&ctx->ctx.cmac, full_mac, &full_len);
         if (ret != 0) {
-            return wc_error_to_psa_status(ret);
+            status = wc_error_to_psa_status(ret);
+            goto cleanup;
         }
     }
     else {
-        return PSA_ERROR_BAD_STATE;
+        status = PSA_ERROR_BAD_STATE;
+        goto cleanup;
     }
 
     if (ctx->mac_length > (size_t)full_len) {
-        return PSA_ERROR_INVALID_ARGUMENT;
+        status = PSA_ERROR_INVALID_ARGUMENT;
+        goto cleanup;
     }
 
     XMEMCPY(mac, full_mac, ctx->mac_length);
     *mac_length = ctx->mac_length;
-    return PSA_SUCCESS;
+    status = PSA_SUCCESS;
+
+cleanup:
+    wc_ForceZero(full_mac, sizeof(full_mac));
+    return status;
 }
 
 psa_status_t psa_mac_sign_finish(psa_mac_operation_t *operation,
