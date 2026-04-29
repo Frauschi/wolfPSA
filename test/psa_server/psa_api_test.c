7df9fb3187c1c4d20607b00211be99b3a1075042
Daniele Lacamera <root@danielinux.net>
2026-04-29 16:27:43 +0200

    Add X448 PSA usability test
    F/3427

--- test/psa_server/psa_api_test.c
+++ test/psa_server/psa_api_test.c
@@ -59,6 +59,7 @@
 #define ED25519_PUBLIC_KEY_BYTES 32u
 #define ED448_PUBLIC_KEY_BYTES   57u
 #define X25519_KEY_BYTES         32u
+#define X448_KEY_BYTES           56u
 
 typedef int (*test_fn_t)(void);
 
@@ -3790,6 +3791,195 @@ static int test_x25519_key_usability(void)
     return TEST_OK;
 }
 
+static int test_x448_key_usability(void)
+{
+    static const uint8_t alice_priv[X448_KEY_BYTES] = {
+        0x9a, 0x8f, 0x49, 0x25, 0xd1, 0x51, 0x9f, 0x57,
+        0x75, 0xcf, 0x46, 0xb0, 0x4b, 0x58, 0x00, 0xd4,
+        0xee, 0x9e, 0xe8, 0xba, 0xe8, 0xbc, 0x55, 0x65,
+        0xd4, 0x98, 0xc2, 0x8d, 0xd9, 0xc9, 0xba, 0xf5,
+        0x74, 0xa9, 0x41, 0x97, 0x44, 0x89, 0x73, 0x91,
+        0x00, 0x63, 0x82, 0xa6, 0xf1, 0x27, 0xab, 0x1d,
+        0x9a, 0xc2, 0xd8, 0xc0, 0xa5, 0x98, 0x72, 0x6b
+    };
+    static const uint8_t bob_priv[X448_KEY_BYTES] = {
+        0x1c, 0x30, 0x6a, 0x7a, 0xc2, 0xa0, 0xe2, 0xe0,
+        0x99, 0x0b, 0x29, 0x44, 0x70, 0xcb, 0xa3, 0x39,
+        0xe6, 0x45, 0x37, 0x72, 0xb0, 0x75, 0x81, 0x1d,
+        0x8f, 0xad, 0x0d, 0x1d, 0x69, 0x27, 0xc1, 0x20,
+        0xbb, 0x5e, 0xe8, 0x97, 0x2b, 0x0d, 0x3e, 0x21,
+        0x37, 0x4c, 0x9c, 0x92, 0x1b, 0x09, 0xd1, 0xb0,
+        0x36, 0x6f, 0x10, 0xb6, 0x51, 0x73, 0x99, 0x2d
+    };
+    static const uint8_t alice_pub_expected[X448_KEY_BYTES] = {
+        0x9b, 0x08, 0xf7, 0xcc, 0x31, 0xb7, 0xe3, 0xe6,
+        0x7d, 0x22, 0xd5, 0xae, 0xa1, 0x21, 0x07, 0x4a,
+        0x27, 0x3b, 0xd2, 0xb8, 0x3d, 0xe0, 0x9c, 0x63,
+        0xfa, 0xa7, 0x3d, 0x2c, 0x22, 0xc5, 0xd9, 0xbb,
+        0xc8, 0x36, 0x64, 0x72, 0x41, 0xd9, 0x53, 0xd4,
+        0x0c, 0x5b, 0x12, 0xda, 0x88, 0x12, 0x0d, 0x53,
+        0x17, 0x7f, 0x80, 0xe5, 0x32, 0xc4, 0x1f, 0xa0
+    };
+    static const uint8_t bob_pub_expected[X448_KEY_BYTES] = {
+        0x3e, 0xb7, 0xa8, 0x29, 0xb0, 0xcd, 0x20, 0xf5,
+        0xbc, 0xfc, 0x0b, 0x59, 0x9b, 0x6f, 0xec, 0xcf,
+        0x6d, 0xa4, 0x62, 0x71, 0x07, 0xbd, 0xb0, 0xd4,
+        0xf3, 0x45, 0xb4, 0x30, 0x27, 0xd8, 0xb9, 0x72,
+        0xfc, 0x3e, 0x34, 0xfb, 0x42, 0x32, 0xa1, 0x3c,
+        0xa7, 0x06, 0xdc, 0xb5, 0x7a, 0xec, 0x3d, 0xae,
+        0x07, 0xbd, 0xc1, 0xc6, 0x7b, 0xf3, 0x36, 0x09
+    };
+    static const uint8_t shared_secret_expected[X448_KEY_BYTES] = {
+        0x07, 0xff, 0xf4, 0x18, 0x1a, 0xc6, 0xcc, 0x95,
+        0xec, 0x1c, 0x16, 0xa9, 0x4a, 0x0f, 0x74, 0xd1,
+        0x2d, 0xa2, 0x32, 0xce, 0x40, 0xa7, 0x75, 0x52,
+        0x28, 0x1d, 0x28, 0x2b, 0xb6, 0x0c, 0x0b, 0x56,
+        0xfd, 0x24, 0x64, 0xc3, 0x35, 0x54, 0x39, 0x36,
+        0x52, 0x1c, 0x24, 0x40, 0x30, 0x85, 0xd5, 0x9a,
+        0x44, 0x9a, 0x50, 0x37, 0x51, 0x4a, 0x87, 0x9d
+    };
+    uint8_t alice_pub[X448_KEY_BYTES];
+    uint8_t bob_pub[X448_KEY_BYTES];
+    uint8_t alice_secret[X448_KEY_BYTES];
+    uint8_t bob_secret[X448_KEY_BYTES];
+    uint8_t generated_pub[X448_KEY_BYTES];
+    size_t alice_pub_len = 0;
+    size_t bob_pub_len = 0;
+    size_t alice_secret_len = 0;
+    size_t bob_secret_len = 0;
+    size_t generated_pub_len = 0;
+    psa_key_id_t alice_key = 0;
+    psa_key_id_t bob_key = 0;
+    psa_key_id_t generated_key = 0;
+    psa_key_attributes_t attrs = psa_key_attributes_init();
+    psa_status_t st;
+
+    psa_set_key_type(&attrs,
+        PSA_KEY_TYPE_ECC_KEY_PAIR(PSA_ECC_FAMILY_MONTGOMERY));
+    psa_set_key_bits(&attrs, 448);
+    psa_set_key_usage_flags(&attrs, PSA_KEY_USAGE_EXPORT | PSA_KEY_USAGE_DERIVE);
+    psa_set_key_algorithm(&attrs, PSA_ALG_ECDH);
+
+    st = psa_import_key(&attrs, alice_priv, sizeof(alice_priv), &alice_key);
+    if (st == PSA_ERROR_NOT_SUPPORTED) {
+        return TEST_SKIPPED;
+    }
+    if (check_status(st, "psa_import_key(X448 alice)") != TEST_OK) {
+        return TEST_FAIL;
+    }
+    st = psa_import_key(&attrs, bob_priv, sizeof(bob_priv), &bob_key);
+    if (check_status(st, "psa_import_key(X448 bob)") != TEST_OK) {
+        (void)psa_destroy_key(alice_key);
+        return TEST_FAIL;
+    }
+
+    st = psa_export_public_key(alice_key, alice_pub, sizeof(alice_pub),
+                               &alice_pub_len);
+    if (check_status(st, "psa_export_public_key(X448 alice)") != TEST_OK) {
+        (void)psa_destroy_key(alice_key);
+        (void)psa_destroy_key(bob_key);
+        return TEST_FAIL;
+    }
+    st = psa_export_public_key(bob_key, bob_pub, sizeof(bob_pub), &bob_pub_len);
+    if (check_status(st, "psa_export_public_key(X448 bob)") != TEST_OK) {
+        (void)psa_destroy_key(alice_key);
+        (void)psa_destroy_key(bob_key);
+        return TEST_FAIL;
+    }
+    if (check_true(alice_pub_len == X448_KEY_BYTES &&
+                   bob_pub_len == X448_KEY_BYTES,
+                   "psa_export_public_key(X448) length") != TEST_OK) {
+        (void)psa_destroy_key(alice_key);
+        (void)psa_destroy_key(bob_key);
+        return TEST_FAIL;
+    }
+    if (check_buf_eq("psa_export_public_key(X448 alice)", alice_pub,
+                     alice_pub_expected, X448_KEY_BYTES) != TEST_OK) {
+        (void)psa_destroy_key(alice_key);
+        (void)psa_destroy_key(bob_key);
+        return TEST_FAIL;
+    }
+    if (check_buf_eq("psa_export_public_key(X448 bob)", bob_pub,
+                     bob_pub_expected, X448_KEY_BYTES) != TEST_OK) {
+        (void)psa_destroy_key(alice_key);
+        (void)psa_destroy_key(bob_key);
+        return TEST_FAIL;
+    }
+
+    st = psa_raw_key_agreement(PSA_ALG_ECDH, alice_key, bob_pub, bob_pub_len,
+                               alice_secret, sizeof(alice_secret),
+                               &alice_secret_len);
+    if (check_status(st, "psa_raw_key_agreement(X448 alice)") != TEST_OK) {
+        (void)psa_destroy_key(alice_key);
+        (void)psa_destroy_key(bob_key);
+        return TEST_FAIL;
+    }
+    st = psa_raw_key_agreement(PSA_ALG_ECDH, bob_key, alice_pub, alice_pub_len,
+                               bob_secret, sizeof(bob_secret),
+                               &bob_secret_len);
+    if (check_status(st, "psa_raw_key_agreement(X448 bob)") != TEST_OK) {
+        (void)psa_destroy_key(alice_key);
+        (void)psa_destroy_key(bob_key);
+        return TEST_FAIL;
+    }
+    if (check_true(alice_secret_len == X448_KEY_BYTES &&
+                   bob_secret_len == X448_KEY_BYTES,
+                   "psa_raw_key_agreement(X448) length") != TEST_OK) {
+        (void)psa_destroy_key(alice_key);
+        (void)psa_destroy_key(bob_key);
+        return TEST_FAIL;
+    }
+    if (check_buf_eq("psa_raw_key_agreement(X448)", alice_secret,
+                     bob_secret, X448_KEY_BYTES) != TEST_OK) {
+        (void)psa_destroy_key(alice_key);
+        (void)psa_destroy_key(bob_key);
+        return TEST_FAIL;
+    }
+    if (check_buf_eq("psa_raw_key_agreement(X448 expected)", alice_secret,
+                     shared_secret_expected, X448_KEY_BYTES) != TEST_OK) {
+        (void)psa_destroy_key(alice_key);
+        (void)psa_destroy_key(bob_key);
+        return TEST_FAIL;
+    }
+
+    st = psa_generate_key(&attrs, &generated_key);
+    if (check_status(st, "psa_generate_key(X448)") != TEST_OK) {
+        (void)psa_destroy_key(alice_key);
+        (void)psa_destroy_key(bob_key);
+        return TEST_FAIL;
+    }
+    st = psa_export_public_key(generated_key, generated_pub,
+                               sizeof(generated_pub), &generated_pub_len);
+    if (check_status(st, "psa_export_public_key(generated X448)") != TEST_OK) {
+        (void)psa_destroy_key(alice_key);
+        (void)psa_destroy_key(bob_key);
+        (void)psa_destroy_key(generated_key);
+        return TEST_FAIL;
+    }
+    if (check_true(generated_pub_len == X448_KEY_BYTES,
+                   "psa_export_public_key(generated X448) length") != TEST_OK) {
+        (void)psa_destroy_key(alice_key);
+        (void)psa_destroy_key(bob_key);
+        (void)psa_destroy_key(generated_key);
+        return TEST_FAIL;
+    }
+
+    st = psa_destroy_key(alice_key);
+    if (check_status(st, "psa_destroy_key(X448 alice)") != TEST_OK) {
+        return TEST_FAIL;
+    }
+    st = psa_destroy_key(bob_key);
+    if (check_status(st, "psa_destroy_key(X448 bob)") != TEST_OK) {
+        return TEST_FAIL;
+    }
+    st = psa_destroy_key(generated_key);
+    if (check_status(st, "psa_destroy_key(X448 generated)") != TEST_OK) {
+        return TEST_FAIL;
+    }
+
+    return TEST_OK;
+}
+
 static int test_hash_verify_rejects_bad_signature(psa_key_type_t type,
                                                   size_t bits,
                                                   psa_algorithm_t alg,
@@ -6571,6 +6761,12 @@ int main(int argc, char** argv)
     if (only == NULL || strcmp(only, "x25519_key_usability") == 0) {
         if (run_named_test("x25519_key_usability",
                            test_x25519_key_usability) == TEST_FAIL) {
+            return TEST_FAIL;
+        }
+    }
+    if (only == NULL || strcmp(only, "x448_key_usability") == 0) {
+        if (run_named_test("x448_key_usability",
+                           test_x448_key_usability) == TEST_FAIL) {
             return TEST_FAIL;
         }
     }
